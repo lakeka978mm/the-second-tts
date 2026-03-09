a06290a74503ae28fc1b3cbaa836b920dd1428cb
lakeka978mm <lakeka978@gmail.com>
2026-03-09 13:54:14 +0800

    fix: update base config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: process.env.GITHUB_PAGES === 'true' ? '/dafei-tts---minimalist-text-to-speech/' : '/',
+      base: process.env.GITHUB_PAGES === 'true' ? '/the-second-tts/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
